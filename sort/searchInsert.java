ebc880111b71280c1e08c5afadd85c334086fbdf
AKASH142005 <720722104060@hicet.ac.in>
2025-03-21 11:07:04 +0530

    Create searchInsert.java

---
+++ sort/searchInsert.java
@@ -0,0 +1,22 @@
+class insert_search_element_array {
+    public static int searchInsert(int[] nums, int target) {
+        int n = nums.length;
+        int l = 0,h = n-1;
+        while(l<=h){
+            int mid = l+(h-l)/2;
+            if(nums[mid]==target) return mid;
+            else if(nums[mid]<target){
+                l=mid+1;
+            }
+            else{
+                h=mid-1;
+            }
+        }
+        return l;
+    }
+  public static void main(String args[]){
+     int nums[]={1,2,4,5,6,7};
+    int target=3;
+    System.out.print(searchInsert(nums,target));
+  }
+}
